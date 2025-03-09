ab1a73f50b3c47d17830b621818af9dfc4fc226a
b0bfranklin <114452596+b0bfranklin@users.noreply.github.com>
2025-03-09 13:53:36 +1100

    Update build-extension.sh

--- docker-build/build-extension.sh
+++ docker-build/build-extension.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+set -e
 
+# Create extension directory structure
+mkdir -p extension/images
+
+# Copy extension files
+cp manifest.json extension/
+cp popup.html extension/
+cp popup.js extension/
+cp background.js extension/
+cp content.js extension/
+cp styles.css extension/browser-extension-styles.css extension/
+
+# Download GitHub icons
+echo "Downloading GitHub icons..."
+curl -s -o extension/images/icon16.png https://raw.githubusercontent.com/JJP123/simpleicons/master/icons/github/github-16.png
+curl -s -o extension/images/icon48.png https://raw.githubusercontent.com/JJP123/simpleicons/master/icons/github/github-48.png
+curl -s -o extension/images/icon128.png https://raw.githubusercontent.com/JJP123/simpleicons/master/icons/github/github-128.png
+
+echo "Extension files prepared in the 'extension' directory"
+echo "You can now load this as an unpacked extension in your browser"
